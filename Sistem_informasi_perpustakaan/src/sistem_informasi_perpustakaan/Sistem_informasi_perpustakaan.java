2b87e6dd0bfe50de6268ac7a1164760086af5242
daniel-kevin <74911912+daniel-kevin@users.noreply.github.com>
2021-10-31 17:55:19 +0700

    Tambahan Class
    Class untuk database connection
    Class untuk Pendaftaran Buku

--- Sistem_informasi_perpustakaan/src/sistem_informasi_perpustakaan/Sistem_informasi_perpustakaan.java
+++ Sistem_informasi_perpustakaan/src/sistem_informasi_perpustakaan/Sistem_informasi_perpustakaan.java
@@ -15,7 +15,8 @@ public class Sistem_informasi_perpustakaan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Main_menu main_menu = new Main_menu();
+        main_menu.setVisible(true);
     }
     
 }
---
+++ Sistem_informasi_perpustakaan/src/sistem_informasi_perpustakaan/connection/db_connection.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistem_informasi_perpustakaan.connection;
+
+import com.mysql.jdbc.Driver;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Daniel
+ */
+public class db_connection {
+    static final String db = "jdbc:mysql://localhost:3306/perpustakaan";
+    static final String user = "root";
+    public static Connection getConnection(){
+        Connection koneksi = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            koneksi = DriverManager.getConnection(db,user,"");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(db_connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return koneksi;
+    }
+}
